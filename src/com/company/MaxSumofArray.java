59e5079b9289cba880f87b292a876519578cc75e
ravipandey-dev <ravipan31.1991@gmail.com>
2021-05-31 17:31:38 +0530

    logic to find size

--- src/com/company/MaxSumofArray.java
+++ src/com/company/MaxSumofArray.java
@@ -9,17 +9,21 @@ public class MaxSumofArray {
 
     private static int findMaxSum(int[] a) {
         int maxSoFar=0;
-        int currentMax=0;
+        int currentMax=0, start=0,end=0,newStart=0;
 
         for(int i=0;i<a.length;i++){
             currentMax=currentMax+a[i];
             if(maxSoFar<currentMax){
                 maxSoFar=currentMax;
+                end=i;
+                start=newStart;
             }
             if(currentMax<0){
                 currentMax=0;
+                newStart=i+1;
             }
         }
+        System.out.println("Array Size is " + (end-start+1));
         return maxSoFar;
     }
 }
